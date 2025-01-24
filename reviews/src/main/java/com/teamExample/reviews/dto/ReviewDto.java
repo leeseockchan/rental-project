26cf4a4a452e1eee4b11b19d0701f06a7ab76cda
leeseokchan <dltrjcks97@gmail.com>
2025-01-24 12:21:27 +0900

    프로젝트 생성(리뷰)

---
+++ reviews/src/main/java/com/teamExample/reviews/dto/ReviewDto.java
@@ -0,0 +1,17 @@
+package com.teamExample.reviews.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class ReviewDto {
+    private Integer id;
+    private int product_id;
+    private int user_id;
+
+
+
+}
