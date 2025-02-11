04ab7c7e8c0fc8784e78eb51a1359df833582b5e
seohyunne <seohyun0012@naver.com>
2025-02-11 11:35:12 +0900

    차량 상세 조회

--- src/main/java/com/road_friends/rentalcar/controller/FastReservationController.java
+++ src/main/java/com/road_friends/rentalcar/controller/FastReservationController.java
@@ -5,8 +5,6 @@ import com.road_friends.rentalcar.dto.FastReservationDto;
 import com.road_friends.rentalcar.service.CarService;
 import com.road_friends.rentalcar.service.FastReservationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
@@ -27,9 +25,21 @@ public class FastReservationController {
     public String getAllCars(Model model){
         List<CarDto> cars = carService.getAllCars();
         model.addAttribute("cars",cars);
-        return "/car/list";
+        return "car/list";
     }
 
+    // 차량 상세 조회
+    @GetMapping("/vehicles/{carId}")
+    public String getCar(@PathVariable ("carId") int id,Model model){
+        try{
+            CarDto car = carService.getCarById(id);
+            model.addAttribute("car",car);
+        }catch(IllegalStateException e){
+            model.addAttribute("message",e.getMessage());
+            return "common/error";
+        }
+        return "car/detail";
+    }
 
 
     // 차량 예약
