f697b9723844843475d31cad14cd937f1391af5d
delftin <resendo13@gmail.com>
2025-02-11 12:12:46 +0900

    UserReviewMapper.java

--- src/main/java/com/road_friends/rentalcar/mapper/UserReviewMapper.java
+++ src/main/java/com/road_friends/rentalcar/mapper/UserReviewMapper.java
@@ -10,23 +10,19 @@ public interface UserReviewMapper {
 
     // 리뷰 작성
     @Insert("INSERT INTO review (user_id, car_name, rating, title, content, created_date) " +
-    "VALUES (#{user_id}, #{car_name}, #{rating}, #{title}, #{content}, #{created_date}")
-    @Options(useGeneratedKeys = true, keyProperty = "id")
+            "VALUES (#{userId}, #{carName}, #{rating}, #{title}, #{content}, #{createdDate})")
     int insertReview(ReviewDTO review);
 
     // 리뷰 수정
-    @Update("UPDATE review SET rating = #{rating}, title = #{title}, content = #{content} " + "WHERE user_id = #{user_id}")
+    @Update("UPDATE review SET rating = #{rating}, title = #{title}, content = #{content} " + "WHERE id = #{id}")
     int updateReview(ReviewDTO review);
 
     // 리뷰 삭제
-    @Delete("DELETE FROM review WHERE user_id = #{user_id} ")
-    int deleteReview(@Param("user_id") int user_id);
+    @Delete("DELETE FROM review WHERE id = #{id} ")
+    int deleteReview(@Param("id") Long id);
 
     // 특정 리뷰 조회
-    @Select("SELECT * FROM review WHERE user_id = #{user_id}")
-    ReviewDTO findById(@Param("user_id") int user_id);
+    @Select("SELECT * FROM review WHERE id = #{id}")
+    ReviewDTO findByReviewId(@Param("id") Long id);
 
-    // 모든 리뷰 조회
-    @Select("SELECT * FROM review")
-    List<ReviewDTO> findAllReviews();
 }
