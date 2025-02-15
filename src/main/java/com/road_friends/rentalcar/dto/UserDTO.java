4d5902c85f4b0303aa698b3afd74a59381c41b2f
delftin <resendo13@gmail.com>
2025-02-15 16:26:52 +0900

    UserDTO.java 추가

---
+++ src/main/java/com/road_friends/rentalcar/dto/UserDTO.java
@@ -0,0 +1,26 @@
+package com.road_friends.rentalcar.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@Data
+public class UserDTO {
+  private Long userNum;
+  private String userId;
+  private String userName;
+  private String userPassword;
+  private String userEmail;
+  private String userPhone;
+  private int userGender;
+  private LocalDate userBirth;
+  private String userAddress;
+  private int userStatus;
+  private boolean enabled = true;
+  // private List<RoleDTO> roles;
+}
