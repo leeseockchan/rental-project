bc2d82f34b8bcf10fc025ba71d2d94f9afd01dda
yesuel <adt04@naver.com>
2025-02-18 17:42:58 +0900

    (관리자) 문의 게시판 : 관리자 페이지 디자인 적용

--- src/main/java/com/road_friends/rentalcar/controller/AdminInquiryController.java
+++ src/main/java/com/road_friends/rentalcar/controller/AdminInquiryController.java
@@ -7,12 +7,8 @@ import com.road_friends.rentalcar.service.AdminInquiryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/api/admin/inquiry")
