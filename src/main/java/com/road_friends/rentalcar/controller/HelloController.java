55776c148abef6018109dc6200a9a6afd8ed52a5
YEWON2848 <yewon3353@gmail.com>
2025-02-19 16:49:59 +0900

    충돌 수정

--- src/main/java/com/road_friends/rentalcar/controller/HelloController.java
+++ src/main/java/com/road_friends/rentalcar/controller/HelloController.java
@@ -6,21 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-<<<<<<< HEAD
   @GetMapping("/")
   public String hello(){
     return "/hello";
   }
-=======
+
   @GetMapping("/hello")
-  public String hello(){
+  public String hello2(){
     return "/hello";
   }
 
-//  출력 확인용. 추후 삭제.
+  // 출력 확인용. 추후 삭제.
   @GetMapping("/con")
   public String layout2(){
     return "/admin/content1";
   }
->>>>>>> origin/feat/admin/page-design
 }
--- src/main/java/com/road_friends/rentalcar/mapper/CarMapper.java
+++ src/main/java/com/road_friends/rentalcar/mapper/CarMapper.java
@@ -2,18 +2,12 @@ package com.road_friends.rentalcar.mapper;
 
 import com.road_friends.rentalcar.dto.CarDto;
 import org.apache.ibatis.annotations.Mapper;
-<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Mapper
 @Repository
-=======
-import java.util.List;
-
-@Mapper
->>>>>>> origin/feat/admin/page-design
 public interface CarMapper {
     List<CarDto> findAll();
     CarDto findById(int carId);
