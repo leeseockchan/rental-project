da86c97d0619fe1358a4fbe75ebee1937b530e9d
seominhee <minheee07@gmail.com>
2025-02-26 21:02:35 +0900

    3000번 포트 cors

--- src/main/java/com/road_friends/rentalcar/controller/CarController.java
+++ src/main/java/com/road_friends/rentalcar/controller/CarController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/cars")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CarController {
 
     private final CarService carService;
