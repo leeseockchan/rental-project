5e7a85e1c3dde409ef206d37fa4f7733a4ae8830
seo minhee <minheee07@gmail.com>
2025-02-27 14:24:44 +0900

    마이페이지 사용자 merge

---
+++ src/main/java/com/road_friends/rentalcar/controller/APIUserController.java
@@ -0,0 +1,41 @@
+package com.road_friends.rentalcar.controller;
+
+
+import com.road_friends.rentalcar.dto.UserDto;
+import com.road_friends.rentalcar.service.APIUserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/user")
+public class APIUserController {
+
+    private final APIUserService apiUserService;
+
+    public APIUserController(APIUserService apiUserService) {
+        this.apiUserService = apiUserService;
+    }
+
+    // 로그인한 사용자의 정보 조회
+    @GetMapping("/{userId}")
+    public UserDto getUserInfo(@PathVariable("userId") String userId) {
+        return apiUserService.getUserInfo(userId);
+    }
+
+    // 로그인한 사용자의 정보 수정
+    @PutMapping("/{userId}")
+    public String updateUserInfo(@PathVariable("userId") String userId, @RequestBody UserDto userDto) {
+        userDto.setUserId(userId);
+        apiUserService.updateUserInfo(userDto);
+        return "회원정보가 수정되었습니다.";
+    }
+
+    // 로그인한 사용자의 탈퇴 (enabled 값을 false 변경)
+    @DeleteMapping("/{userId}")
+    public String disableUser(@PathVariable("userId") String userId) {
+        apiUserService.disableUser(userId);
+        return "회원 탈퇴처리가 완료되었습니다.";
+    }
+
+
+
+}
