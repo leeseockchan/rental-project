42b8507183619e51ed55403f6d92e61e679ff982
seo minhee <minheee07@gmail.com>
2025-02-28 10:27:57 +0900

    컨트롤러 엔드포인트 경로 수정

--- src/main/java/com/road_friends/rentalcar/controller/AdminCarController.java
+++ src/main/java/com/road_friends/rentalcar/controller/AdminCarController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @Controller
-@RequestMapping("/api/admin/vehicles")
+@RequestMapping("/admin/vehicles")
 public class AdminCarController {
 
     @Autowired
