4a3df3dbe11b5482e26a972c40a4a78c57bca25a
seo minhee <minheee07@gmail.com>
2025-02-28 14:53:11 +0900

    예약관리 병합, cors 제한해제, 관리자 gnb 수정

---
+++ src/main/java/com/road_friends/rentalcar/dto/AdminModelDto.java
@@ -0,0 +1,26 @@
+package com.road_friends.rentalcar.dto;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminModelDto {
+    private String modelId;  // model_id -> modelId
+
+    private String modelBrand;
+
+    private String modelName;
+
+    private String modelCategory;
+
+    private String modelSeateNum;
+
+    private String modelTransmission;
+
+    private int modelAmountHour;
+
+    private int modelAmountDay;
+
+
+}
