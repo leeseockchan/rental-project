cf1c8a9bc5e2bd8f206dacf968a8a781853582d1
seo minhee <minheee07@gmail.com>
2025-02-28 14:40:41 +0900

    dto 이름 변경

--- src/main/java/com/road_friends/rentalcar/dto/AdminFastReservationDto.java
+++ src/main/java/com/road_friends/rentalcar/dto/AdminFastReservationDto.java
@@ -6,7 +6,7 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Data
-public class FastReservationDto {
+public class AdminFastReservationDto {
   private int carId;
   private String modelName;
   private int rentalLocation;
