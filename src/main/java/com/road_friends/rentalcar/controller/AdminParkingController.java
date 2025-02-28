e185ae9c8b58749066fd07582a102ec8374d3111
seo minhee <minheee07@gmail.com>
2025-02-28 09:22:33 +0900

    주차장 지역조회 수정

--- src/main/java/com/road_friends/rentalcar/controller/AdminParkingController.java
+++ src/main/java/com/road_friends/rentalcar/controller/AdminParkingController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -31,15 +32,13 @@ public class AdminParkingController {
 
     // 주차 도/시 리스트
     private List<String> getProvinceList() {
-        return List.of("서울특별시", "경기도", "충청북도", "충청남도",
-                "경상북도", "경상남도", "전라북도", "전라남도", "제주도");
+        return List.of("서울특별시", "인천광역시" ,"경기도", "충청남도", "충청북도",
+                "경상북도", "경상남도", "강원도", "전라북도", "전라남도", "제주도");
     }
 
     // 주차장 전체 목록
     @GetMapping
     public String showAllParking(Model model) {
-        List<AdminParkingDto> parkingList = adminParkingService.findAll();
-        model.addAttribute("parkingList", parkingList);
         model.addAttribute("provinceList", getProvinceList()); // 시/도 리스트 추가
         return "parking_page/list";
     }
@@ -56,7 +55,9 @@ public class AdminParkingController {
     @GetMapping("/add")
     public String addParking(Model model) {
         model.addAttribute("provinceList", getProvinceList());
-        model.addAttribute("parkingDto", new AdminParkingDto());
+        model.addAttribute("adminParkingDto", new AdminParkingDto());
+        model.addAttribute("provinceList", adminParkingService.getAllProvinces());
+        model.addAttribute("districtList", new ArrayList<String>());
         return "parking_page/add";
     }
 
--- src/main/java/com/road_friends/rentalcar/mapper/AdminParkingMapper.java
+++ src/main/java/com/road_friends/rentalcar/mapper/AdminParkingMapper.java
@@ -26,15 +26,15 @@ public interface AdminParkingMapper {
     List<String> getDistrictsByProvince(@Param("province") String province);
     @Select("SELECT * FROM parking WHERE parking_district = #{district}")
     List<AdminParkingDto> findByDistrict(String district);
-//    주차장 목록 메소드
+    //    주차장 목록 메소드
     List<AdminParkingDto> findAll();
-//    주차장 상세보기
+    //    주차장 상세보기
     AdminParkingDto findByParking(int parkingId);
-//    주차장 추가
+    //    주차장 추가
     void addParking(AdminParkingDto adminParkingDto);
-//    주차장 수정
+    //    주차장 수정
     void modifyParking(AdminParkingDto adminParkingDto);
-//    주차장 삭제
+    //    주차장 삭제
     void deleteParking(int parkingId);
 
 
