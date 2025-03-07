36b0a4e1bb2fbfb94f9a072f9314b6517b8a1a7a
yesuel <adt04@naver.com>
2025-03-07 15:22:56 +0900

    (관리자) 리뷰 관리 : 리스트 검색 기능 수정.

--- src/main/resources/static/js/review/review-filter.js
+++ src/main/resources/static/js/review/review-filter.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelector(".btn-update").addEventListener("click", function () {
-        const year = document.getElementById("filter-year").value;
-        const month = document.getElementById("filter-month").value;
-        const url = `/admin/review?year=${year}&month=${month}&page=1`;
-        window.location.href = url;
-    });
+    document.querySelector(".btn-update").addEventListener("click", searchReviews);
 });
 
 function searchReviews() {
     const year = document.getElementById("filter-year").value;
     const month = document.getElementById("filter-month").value;
-    window.location.href = `/admin/review?page=1&size=10&year=${year}&month=${month}`;
+
+    let url = `/admin/review?page=1&size=10`;
+
+    if (year) url += `&year=${year}`;
+    if (month) url += `&month=${month}`;
+
+    window.location.href = url;
 }
