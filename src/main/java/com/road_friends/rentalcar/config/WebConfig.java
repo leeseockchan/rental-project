5de50ec608f5f0ac85e983363812bc8e8639a1ba
seominhee <minheee07@gmail.com>
2025-03-10 02:26:20 +0900

    관리자 gnb 경로 수정, 로고삽입

--- src/main/java/com/road_friends/rentalcar/config/WebConfig.java
+++ src/main/java/com/road_friends/rentalcar/config/WebConfig.java
@@ -10,5 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/images/notice/**") // 클라이언트에서 접근할 URL 패턴
             .addResourceLocations("file:///E:/images/notice/"); // 실제 파일 저장 경로
+
+    // 내부 정적 리소스 폴더 (static/images/)에서 이미지 제공
+    registry.addResourceHandler("/images/**")
+            .addResourceLocations("classpath:/static/images/");
   }
 }
