a06483c437149617aa209546ac92c6e083e2ad03
seo minhee <minheee07@gmail.com>
2025-03-18 14:20:44 +0900

    user-update(이름 검색)

--- src/main/java/com/road_friends/rentalcar/dto/UserDto.java
+++ src/main/java/com/road_friends/rentalcar/dto/UserDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
@@ -43,5 +44,5 @@ public class UserDto {
 
     private boolean enabled = true;
 
-//    private List<RoleDTO> roles;
+    private List<RoleDto> roles;
 }
