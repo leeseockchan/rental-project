6ded8d9b3b8b337a3fafd5a9a7aa193def74b37e
seominhee <minheee07@gmail.com>
2025-03-22 15:10:26 +0900

    JsonProperty 삭제

--- src/main/java/com/road_friends/rentalcar/dto/UserDto.java
+++ src/main/java/com/road_friends/rentalcar/dto/UserDto.java
@@ -12,34 +12,24 @@ import java.util.List;
 @Setter
 @Data
 public class UserDto {
-    @JsonProperty("user_num")
     private Long userNum;
 
-    @JsonProperty("user_id")
     private String userId;
 
-    @JsonProperty("user_name")
     private String userName;
 
-    @JsonProperty("user_password")
     private String userPassword;
 
-    @JsonProperty("user_email")
     private String userEmail;
 
-    @JsonProperty("user_phone")
     private String userPhone;
 
-    @JsonProperty("user_gender")
     private int userGender;
 
-    @JsonProperty("user_birth")
     private LocalDate userBirth;
 
-    @JsonProperty("user_address")
     private String userAddress;
 
-    @JsonProperty("user_status")
     private int userStatus;
 
     private boolean enabled = true;
